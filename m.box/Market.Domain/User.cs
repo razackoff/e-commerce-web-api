c58d7ba0cb5fdab64ed5fd098d246b42c6769629
Firuz <razackoff@gmail.com>
2022-10-03 20:32:40 +0500

    Добавьте файлы проекта.

---
+++ m.box/Market.Domain/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Market.Domain
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public string Region { get; set; }
+        public string Locality { get; set; }
+        public Gender UserGender { get; set; } 
+        public string Password { get; set; }
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+    }
+}
