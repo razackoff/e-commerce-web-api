9edd6c5a605563640984081d60c8f8d61e62c894
firuz <razackoff@gmail.com>
2023-01-10 12:50:56 +0500

    Создать класс продукт.

---
+++ m.box/Market.Domain/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Market.Domain
+{
+    public class Product
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public Product(Guid userId, string name, string description, decimal price)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+    }
+}
